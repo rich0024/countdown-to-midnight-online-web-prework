6ecfc173b6960fe4222a35d1f7cb2da35e55ac0c
Richard Yance <yance0022@gmail.com>
2018-10-18 06:53:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
 def countdown
+  def countdown(n)
+  phrase = "HAPPY NEW YEAR!"
+  counter = 0
+  
+  while counter < n do
+    puts "#{n} SECOND(S)!"
+     n -= 1
+  end
+  return phrase
+end
+ def countdown_with_sleep(n)
+    phrase = "HAPPY NEW YEAR!"
+  counter = 0
+  
+  while counter < n do
+    puts "#{n} SECOND(S)!"
+     n -= 1
+     sleep(1)
+  end
+  return phrase
 end
